d7a35d362aa2b96c6fd2716f0e39818a92ff2bdf
Vignesh <paramasivamvignesh2002@gmail.com>
2024-08-20 18:13:01 +0530

    Create circle circumference.py

---
+++ circle circumference.py
@@ -0,0 +1,12 @@
+# Calculate the circumference of a circle with diameter d.
+
+# create the function
+def calculate_circle_circumference(diameter):
+#check the circle circumference formula 
+    circle_circumference = 3.14 * diameter
+#create the return statement 
+    return circle_circumference
+#answer are required 
+d = int(input("Enter the diameter : "))
+# print the function
+print(calculate_circle_circumference(diameter = d),"is a circle circumference value.") 

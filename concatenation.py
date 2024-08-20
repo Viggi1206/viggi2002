46a7356d2cb4423ff245403c04ca80a588e34619
Vignesh <paramasivamvignesh2002@gmail.com>
2024-08-20 17:35:49 +0530

    Create concatenation.py

---
+++ concatenation.py
@@ -0,0 +1,13 @@
+# Write a python function to check add the two strings it means concatenation.
+
+# Create the function and use 'def'
+def concatenates(value1,value2):
+# Check the concatenation with two inputs.
+    add_two_strings = value1 +  " " + value2
+#Create the return statement 
+    return add_two_strings
+#Two answers are required for name and father name. 
+name = input("Enter the your name : ")
+father_name = input("Enter the your father name : ")
+# print and call the function
+print('My name is',concatenates(value1 = father_name,value2 = name))

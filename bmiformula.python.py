fb1934a699d0a2986a98d3a18a5d22e310ad694a
Vignesh <paramasivamvignesh2002@gmail.com>
2024-08-20 15:25:48 +0530

    Add files via upload

---
+++ bmiformula.python.py
@@ -0,0 +1,6 @@
+def bmi():
+    print("The bmi calculations value is ",formula)
+weight = int(input("Enter the weight : "))
+height = int(input("Enter the height : "))
+formula = height / weight**2
+bmi()

b2590f17c7afdb49ef5ef14555fa5e96e5bdde8e
Vignesh <paramasivamvignesh2002@gmail.com>
2024-08-20 16:46:06 +0530

    Add files via upload

---
+++ areaofrectangle.python.py
@@ -0,0 +1,7 @@
+def area_of_rectangle(multipilcation):
+    print(multipilcation,"is a Area of Rectangle value.")
+
+width = int(input("Enter the width value : ")) 
+length = int(input("Enter the length value : "))
+multipilcation = length * width
+area_of_rectangle(multipilcation)

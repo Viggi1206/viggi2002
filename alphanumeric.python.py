b168285d6347058cbddf7154a2545fbd9789790b
Vignesh <paramasivamvignesh2002@gmail.com>
2024-08-20 15:28:42 +0530

    Add files via upload

---
+++ alphanumeric.python.py
@@ -0,0 +1,8 @@
+def numeric(n):
+    if a >= "b":
+        print(b," is a Alphanumeric.")
+    else:
+        print(b," is a Not Alphanumeric.")
+a = input("enter the value: ")
+b = a.isalnum()
+numeric(b)

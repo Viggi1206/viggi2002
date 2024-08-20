c034ef189f3478d4682a9a4f28950fd74430ced4
Vignesh <paramasivamvignesh2002@gmail.com>
2024-08-20 16:08:02 +0530

    Update and rename alphanumeric.python.py to alphanumeric.py

---
+++ alphanumeric.py
@@ -0,0 +1,15 @@
+#  Write a Python function to check if a given string contains only alphanumeric characters.
+
+#create the function
+def alphanumeric(value):
+# check the string manipulation
+    conversion = user_value.isalnum()
+#check the condition  
+    if value >= "conversion":
+        print("This is a alpanumeric value") 
+    else:
+        print("This is not a alphanumeric value") 
+# one answer are required
+user_value = input("Enter the value : ")
+# check the function call
+numeric(value = user_value)
--- alphanumeric.python.py
+++
@@ -1,8 +0,0 @@
-def numeric(n):
-    if a >= "b":
-        print(b," is a Alphanumeric.")
-    else:
-        print(b," is a Not Alphanumeric.")
-a = input("enter the value: ")
-b = a.isalnum()
-numeric(b)

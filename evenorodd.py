e840d83de3ffd94dc8348cac0da038c59a4e5959
Vignesh <paramasivamvignesh2002@gmail.com>
2024-08-20 16:36:19 +0530

    Update and rename evenorodd.python.py to evenorodd.py

--- evenorodd.py
+++ evenorodd.py
@@ -1,19 +1,22 @@
+# Write the Python funtion to check odd or add.
+
+# Create the funtion and use 'def'
 def to_check_odd_or_even(number):
+# Check the 'nested if' condition
     if (0 < number <= 1000):
         if (0 < number <= 500):
             if number % 2 != 0:
                 print(number,"is a Odd number.")
             else:
                 print(number,"is not a Odd number.")
-       
-            
         else:
             if number % 2 == 0:
                 print(number,"is a Even number.")
             else:
                 print(number,"is not a Even number.")
-       
     else:
         print(number,"Please enter the number above 0 to below 1000.")
-user_number = int(input("Enter the number: "))        
+# one answer are required        
+user_number = int(input("Enter the number: ")) 
+# Call the funtion
 to_check_odd_or_even(number = user_number)

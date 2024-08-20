b0ba47f06a5cfa1a6577c862aaf4e9ae1464dd18
Vignesh <paramasivamvignesh2002@gmail.com>
2024-08-20 17:21:13 +0530

    Update and rename areaofrectangle.python.py to areaofrectangle.py

---
+++ areaofrectangle.py
@@ -0,0 +1,13 @@
+# Write a python function to check the area of rectangle.
+
+# Create the function and use 'def'
+def area_of_rectangle(value1,value2):
+# Check the area of rectangle formula 
+    multipilcation = length * width
+# Create the return statement
+    return multipilcation
+# Two answer are required for width and length
+width = int(input("Enter the width value : ")) 
+length = int(input("Enter the length value : "))
+# print and call the function
+print(area_of_rectangle(value1 = width,value2 = length),"is a area of rectangle value.")
